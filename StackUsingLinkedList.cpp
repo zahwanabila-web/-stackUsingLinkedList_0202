19a45d1415b5bf285b511b0869667ecd0e2611cc
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-12 11:00:03 +0700

    commit8

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -97,6 +97,11 @@ int main()
             cin >> value;
             stack.push(value); //push the entered value onto the stack
             break;
+        case 2:
+            if (! stack.isEmpty())
+            {
+                stack.pop(); //pop the top element from the stack
+            }
 
         }
     }
