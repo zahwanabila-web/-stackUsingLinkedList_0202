d32f2beb46bdd083da0254cf3dfc09677aea742e
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-12 10:37:36 +0700

    commit2

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -14,3 +14,26 @@ public:
     }
 };
 
+//stack class
+class stack
+{
+private:
+    Node *top; //pointer to the top node of the stack
+
+public:
+    stack()
+    {
+        top = NULL;
+
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "push value: " << value << endl;
+        return value;
+    }
+    
+};
